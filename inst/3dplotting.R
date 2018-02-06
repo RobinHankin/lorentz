afa723349c48d8483e9269d1fb0f3c84ee385217
RobinHankin <hankin.robin@gmail.com>
2018-02-06 15:34:17 +1300

    example file for nice 3D plots of noncommutativity and nonassociativity of Lorentz velocity addition

---
+++ inst/3dplotting.R
@@ -0,0 +1,21 @@
+library(sphereplot)
+library(gyrogroup)
+
+
+u <- as.3vel(c(0,0,0.8))
+v <- r3vel(3000,0.9)
+w <- as.3vel(c(0,0.8,0))
+
+n1 <- (u+v)-(v+u)            # noncommutativity
+n2 <- (u+(v+w)) - ((u+v)+w)  # nonassociativity
+
+
+
+rgl.sphgrid()
+rgl.sphpoints(car2sph(v))
+
+rgl.sphgrid()
+rgl.sphpoints(car2sph(n1))
+
+rgl.sphgrid()
+rgl.sphpoints(car2sph(n2))
