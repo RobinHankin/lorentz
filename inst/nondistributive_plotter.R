910711d335b45f052fcb62bdb85e41f16d53f9f4
RobinHankin <hankin.robin@gmail.com>
2018-02-11 16:11:55 +1300

    streamline calls to plotting functions

--- inst/nondistributive_plotter.R
+++ inst/nondistributive_plotter.R
@@ -3,26 +3,28 @@
 library("gyrogroup")
 
 
-# plot commands start
+# following three-velocities closely match those of comm_fail.Rd:
+jj <- as.3vel(c(0.4,-0.2,0))
+kk <- as.3vel(c(-0.4,0.6,0))
+kk1 <- as.3vel(c(-0.4,0.3,0))
+
+u <- as.3vel(c(0.5,0,0))
+v <- jj + kk*seq(from=0,to=3,len=28)
+w <- as.3vel(c(0.8,-0.4,0))
 
 pdf(file="comm_fail1.pdf")
-  comm_fail1(
-    u =  as.3vel(c(0.4,0,0)),
-    v = as.3vel(c(0.4,-0.2,0)) + as.3vel(c(-0.4,0.6,0)) * seq(from=0,to=3,len=28)
-)
+comm_fail1(u=u, v=v)
+dev.off()
 
 pdf(file="comm_fail2.pdf")
-comm_fail2(
-    u =  as.3vel(c(0.4,0,0)),
-    v = as.3vel(c(0.4,-0.2,0)) + as.3vel(c(-0.4,0.6,0)) * seq(from=0,to=3,len=28)
-)
+comm_fail2(u=u, v=v)
 dev.off()
 
 pdf(file="ass_fail.pdf")
 ass_fail(
-    u = as.3vel(c(0.5,0,0)),
-    v = as.3vel(c(0.4,-0.2,0)) + as.3vel(c(-0.4,0.3,0)) * seq(from=0,to=3,len=28),
-    w = as.3vel(c(0.8,-0.4,0)),
-    bold=23
-    )
+    u=u, 
+    v = jj + kk1 * seq(from=0,to=3,len=28),
+    w = w, bold=23
+)
 dev.off()
+
