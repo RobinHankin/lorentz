78d18ee8ebf208e4bd77a3297f0ad7b0c1a6eb79
RobinHankin <hankin.robin@gmail.com>
2018-12-12 08:39:12 +1300

    stress energy tensor functionality

---
+++ R/stress_energy_tensor.R
@@ -0,0 +1,10 @@
+`perfectfluid` <- function(rho,p=0,u=0){
+  p <- p/sol()^2
+  crossprod(as.4vel(u))*(rho+p) +  p*eta(downstairs=FALSE)
+}
+
+`dust` <- function(rho,u=0){perfectfluid(rho,p=0,u=u)}
+
+`transform_uu` <-  function(TT,B){ tensor(B,tensor(B,TT,2,2),2,2) }
+`transform_ud` <-  function(TT,B){ tensor(B,tensor(B,TT,1,2),2,2) }
+`transform_dd` <-  function(TT,B){ tensor(B,tensor(B,TT,1,2),1,2) }
