8cf25d2595eb9dec23bb7b2d652cb0d5351e1458
RobinHankin <hankin.robin@gmail.com>
2018-12-12 13:36:29 +1300

    photon gas functionality

--- R/stress_energy_tensor.R
+++ R/stress_energy_tensor.R
@@ -4,6 +4,7 @@
 }
 
 `dust` <- function(rho,u=0){perfectfluid(rho,p=0,u=u)}
+`photongas` <- function(rho,u=0){perfectfluid(rho,p=rho/3,u=u)}
 
 `transform_uu` <-  function(TT,B){ tensor(B,tensor(B,TT,2,2),2,2) }
 `transform_ud` <-  function(TT,B){ tensor(B,tensor(B,TT,1,2),2,2) }
