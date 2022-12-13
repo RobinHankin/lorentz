0ecf0f06708932fe817aec2baa53b36ec581be01
Robin Hankin <hankin.robin@gmail.com>
2022-12-14 11:36:07 +1300

    celerity (experimental)

--- R/celerity.R
+++ R/celerity.R
@@ -1,4 +1,11 @@
-`cel_to_vel` <- function(x){}
-`vel_to_cel` <- function(x){}
+`cel_to_vel` <- function(x){
+  x <- unclass(x)
+  sweep(x,1,sqrt(1+rowSums(x^2)/sol()^2),"/")
+}
+
+`vel_to_cel` <- function(x){
+  out <- as.4vel(x)
+  as.3cel(unclass(out)[,-1])
+}
 
 
